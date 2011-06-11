54e6116151ecca821845cc9b839834323fe50c33
shtirlic <serg@podtynnyi.com>
2011-06-11 22:57:53 +0400

    Code clean

--- lib/niceid.rb
+++ lib/niceid.rb
@@ -5,24 +5,24 @@ module NiceId
     DEFAULT_SIZE = 3
 
     def initialize(how_many = DEFAULT_SIZE)
-      @key = ''
+      key = ''
       @summ = 0
       how_many.times do
         a = rand VARS_NUM.size
         b = rand VARS_LET.size
-        @key<<VARS_NUM[a]+VARS_LET[b]
+        key<<VARS_NUM[a]+VARS_LET[b]
       end
-      @key=@key.chop << calc
+      @key = key.chop << calc(key)
     end
     def to_s
       @key
     end
     private
     def correct?
-      @key[-1..-1] == calc
+      @key[-1..-1] == calc(@key)
     end
-    def calc
-      @sum = @key.chop.scan(/./).map{|a| VARS_NUM.index(a)||VARS_LET.index(a)||0 }
+    def calc(key)
+      @sum = key.chop.scan(/./).map{|a| VARS_NUM.index(a)||VARS_LET.index(a)||0 }
       VARS_LET.at @sum.inject(:+) % VARS_LET.size
     end
   end
